cf24fd15797119c679b4d1ff3806df8516d44e9a
Emerson Lino <powelllino@gmail.com>
2018-11-19 12:40:55 -0300

    generate topo e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
